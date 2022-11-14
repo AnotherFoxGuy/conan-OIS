59bcb15a86f8332800e8af86a2e7df1d3ed47c74
Edgar <Edgar@AnotherFoxGuy.com>
2022-11-14 19:00:47 +0100

    :wrench: Added support for building with conan v2

--- conanfile.py
+++ conanfile.py
@@ -1,32 +1,40 @@
-from conans import ConanFile, CMake, tools
-from conans.tools import os_info, SystemPackageTool
+from conan import ConanFile
+from conan.tools.files import collect_libs
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps, cmake_layout
+from conan.tools.system import package_manager
 
 
 class OisConan(ConanFile):
     name = "ois"
-    version = "1.4"
+    version = "1.4.1"
     license = "zlib"
     author = "Edgar Edgar@AnotherFoxGuy.com"
     url = "https://github.com/AnotherFoxGuy/conan-OIS/"
     description = "Object oriented Input System"
     topics = ("Input", "System")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package"
     exports_sources = [
         "includes/*",
         "src/*",
         "CMakeLists.txt"
     ]
 
+    def layout(self):
+        cmake_layout(self)
+
     def requirements(self):
-        if os_info.is_windows:
+        if self.settings.os == "Windows":
             self.requires("directx-sdk/9.0@anotherfoxguy/stable")
 
     def system_requirements(self):
-        if os_info.is_linux:
-            if os_info.with_apt:
-                installer = SystemPackageTool()
-                installer.install("libx11-dev")
+        package_manager.Apt(self).install(["libx11-dev"])
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["OIS_BUILD_DEMOS"] = "OFF"
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -39,4 +47,4 @@ class OisConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.includedirs = ['include', 'include/OIS']
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
